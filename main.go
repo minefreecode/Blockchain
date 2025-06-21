ed98cd6b2ce5df21118a791c4522ae1004e6088b
aleksey <aleksey.work.job@gmail.com>
2025-06-21 12:32:11 +0300

    [feature] Adding block

--- main.go
+++ main.go
@@ -5,11 +5,12 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"strconv"
+	"time"
 )
 
 // Block Блок для хеширования данных
 type Block struct {
-	Pos       int
+	Pos       int // Номер позиции в блокчейне
 	Data      BookCheckout
 	Timestamp string
 	Hash      string // Хеш-значение в блоке. Получается из Data + Время + Pos
@@ -46,14 +47,46 @@ func (b *Block) generateHash() {
 	b.Hash = hex.EncodeToString(hash.Sum(nil)) //Получение строки хеша в види строки и заненсение его в блок
 }
 
+// Проверка, являет ил хеш правильным
+func (b *Block) validateHash(hash string) bool {
+	b.generateHash()      //Сгенерировать новый хеш
+	return b.Hash == hash //Проверка совпадения хешей
+}
+
 func (bc *Blockchain) AddBlock(data BookCheckout) {
 	prevBlock := bc.blocks[len(bc.blocks)-1]
 	block := CreateBlock(prevBlock, data)
-
+	if validBlock(block, prevBlock) {
+		bc.blocks = append(bc.blocks, block)
+	}
 }
 
+// CreateBlock Создание нового блока
 func CreateBlock(prevBlock *Block, data BookCheckout) *Block {
-	block := new(Block)
+	block := new(Block)                   // Выделение памяти для структуры
+	block.Pos = prevBlock.Pos + 1         // Установка позиции из предыдущего блока
+	block.Data = data                     //Установка данных о проверке книги
+	block.Timestamp = time.Now().String() // Время
+	block.PrevHash = prevBlock.Hash       //Сохранение хеша предыдущего блока
+	block.generateHash()                  // Генерация нового хеша для блока
 
 	return block
+}
+
+// Валидация блока
+// Проверки:
+// 1)Валиден ли хег предыдущего блока, есть ли связь со следущим по хеши;
+// 2)проверка хеша самого блока
+// 3) Проверка позиции Pos предыдущего блока
+func validBlock(block *Block, prevBlock *Block) bool {
+	if block.PrevHash != prevBlock.Hash {
+		return false
+	}
+	if !block.validateHash(block.Hash) {
+		return false
+	}
+	if prevBlock.Pos+1 != block.Pos {
+		return false
+	}
+	return true
 }
